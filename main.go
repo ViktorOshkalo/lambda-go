eef47aa15819e9977de200a1e7e9793dfb52475b
Viktor Oshkalo <vvicmail@gmail.com>
2025-01-12 22:19:44 +0200

    init

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+var (
+	s3Client *s3.Client
+)
+
+func init() {
+	// Initialize the S3 client outside of the handler, during the init phase
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	s3Client = s3.NewFromConfig(cfg)
+}
+
+type Params struct {
+	BucketName string `json:"bucketName"`
+	ObjectKey  string `json:"objectKey"`
+}
+
+func handler(ctx context.Context, event Params) (float64, error) {
+	log.Printf("processing starting with params: bucket name: %s, file name: %s", event.BucketName, event.ObjectKey)
+
+	result, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(event.BucketName),
+		Key:    aws.String(event.ObjectKey),
+	})
+
+	if err != nil {
+		var noKey *types.NoSuchKey
+		if errors.As(err, &noKey) {
+			log.Printf("can't get object %s from bucket %s. no such key exists.\n", event.ObjectKey, event.BucketName)
+			err = noKey
+		} else {
+			log.Printf("couldn't get object %v:%v: %v\n", event.BucketName, event.ObjectKey, err)
+		}
+		return 0, err
+	}
+
+	defer result.Body.Close()
+
+	return CalculateGPAAverage(result.Body)
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+// helper
+func CalculateGPAAverage(r io.Reader) (float64, error) {
+	reader := csv.NewReader(r)
+
+	header, err := reader.Read()
+	if err != nil {
+		return 0, fmt.Errorf("failed to read header: %v", err)
+	}
+
+	if header[1] != "GPA" {
+		return 0, fmt.Errorf("CSV file must have GPA as a second column")
+	}
+
+	var totalGPA float64
+	var count int
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, fmt.Errorf("failed to read row: %v", err)
+		}
+
+		gpa, err := strconv.ParseFloat(record[1], 64)
+		if err != nil {
+			return 0, fmt.Errorf("invalid GPA value %q: %v", record[1], err)
+		}
+
+		totalGPA += gpa
+		count++
+	}
+
+	if count == 0 {
+		return 0, fmt.Errorf("no valid GPA data found")
+	}
+
+	return totalGPA / float64(count), nil
+}
